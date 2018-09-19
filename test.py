5d01b8275d5f3f5ae856b5d6e6b57b7840b4a06d
kaggwachristopher <chrisduke.999@gmail.com>
2018-09-19 13:21:26 +0300

    update unittest[#160420163] [#160421514]

---
+++ test.py
@@ -0,0 +1,22 @@
+import unittest
+
+
+class Api(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_all_orders_getter(self):
+        pass
+
+    def test_single_order_getter(self):
+        pass
+
+    def test_order_updater(self):
+        pass
+
+    def test_place_order(self):
+        pass
